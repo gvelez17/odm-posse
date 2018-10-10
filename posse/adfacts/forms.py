3624bd65db9e61194264407dd0122fa603de45d5
root <root@debian>
2018-10-10 09:19:55 +0000

    really simple search form

--- posse/adfacts/forms.py
+++ posse/adfacts/forms.py
@@ -1,5 +1,5 @@
-from .models import Ad
-from django.forms import ModelForm
+from .models import Ad, Org
+from django.forms import ModelForm, ChoiceField
 from simple_search import search_form_factory
 
 #AdSearchForm = search_form_factory(Ad.objects.all(),
@@ -13,5 +13,11 @@ class AdSearchForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(AdSearchForm, self).__init__(*args, **kwargs)
+        ads_for = list(set([ad.candidate_or_initiative for ad in Ad.objects.all()]))
+        options = [('','')] + [(ad_name, ad_name) for ad_name in ads_for]
+
+        self.fields['candidate_or_initiative'] = ChoiceField(
+               choices = options
+        )
         self.fields['candidate_or_initiative'].required = False
         self.fields['race'].required = False
--- posse/adfacts/views.py
+++ posse/adfacts/views.py
@@ -19,22 +19,34 @@ class OrgDetailView(generic.DetailView):
 class AdDetailView(generic.DetailView):
     model = Ad
 
-class AdFormView(FormView):
-    template_name = 'search.html'
-    form_class = AdSearchForm
-
-    def form_valid(self, form):
-        import pdb; pdb.set_trace()
-
+#class AdFormView(FormView):
+#    template_name = 'search.html'
+#    form_class = AdSearchForm
+#
+#    def form_valid(self, form):
+#        import pdb; pdb.set_trace()
+#
 
 def search(request):
-    form = AdSearchForm(request.GET or {})
+    form = AdSearchForm(request.POST or {})
     if form.is_valid():
-        results = form.get_queryset()
+        if form.data['race']:
+           results = Ad.objects.filter(race=form.data['race'], 
+                              candidate_or_initiative=form.data['candidate_or_initiative'],
+                              support_oppose=form.data['support_oppose']
+                             )
+        else:
+           results = Ad.objects.filter( 
+                              candidate_or_initiative=form.data['candidate_or_initiative'],
+                              support_oppose=form.data['support_oppose']
+                             )
+        results_header = "Results:"
     else:
         results = Ad.objects.none()
+        results_header = ""
 
     return render(request, 'search.html', {
        'form': form,
        'results': results,
+       'results_header': results_header
     })
